0e700f40e7236cca235513bbcc48e13a01caafa2
Mehmet Reşit Akın <103512411+resitakinn@users.noreply.github.com>
2024-11-10 06:51:06 +0300

    app-js-dosyası-oluşturuldu-include-edildi-işlem-yapıldı

---
+++ assets/js/app.js
@@ -0,0 +1,75 @@
+ // let input = prompt("Mesaj nedir");
+ // console.log(input); 
+
+ // console.log("Merhaba");
+// değişkenler
+// let sayi1 = prompt("1.Sayı giriniz");
+// const sayi2 = prompt("2.Sayı giriniz");
+// var sayi3 = prompt("3.Sayı giriniz");
+
+// console.log("Sayı 1 =", sayi1);
+// console.log("Sayı 2 =", sayi2);
+// console.log("Sayı 3 =", sayi3);
+
+// const değeri başalngıç değeri olması lazım
+// let sayi1 = 43;
+// const sayi2 = 12; 
+// var sayi3 = 14;
+
+// sayi1 = 12;
+// sayi3 = 12;
+
+
+// console.log("Sayı 1 =", sayi1);
+// console.log("Sayı 2 =", sayi2);
+// console.log("Sayı 3 =", sayi3);
+
+
+// console.log = ("ben App.js den geldim");
+
+// let dersler = [];
+
+ // console.log("Dersler", dersler ,"dersler Uzunluğu", dersler.length);
+
+// dersler.push("PHP"); // Arrayın Sonundan ekler
+// dersler.push("HTML");
+// dersler.push("CSS");
+// dersler.unshift("C#");// Arrayın En başından başlar
+
+ // console.log("Dersler", dersler, "dersler Uzunluğu", dersler.length);
+
+// dersler.pop(); // son elemanı siler
+// dersler.shift(); // ilk elemanı siler
+   
+// console.log("Dersler", dersler, "dersler Uzunluğu", dersler.length);
+
+// dersler.push("Java","Python");
+
+// console.log("Dersler", dersler, "dersler Uzunluğu", dersler.length); 
+
+// let cSharpIndex = dersler.findIndex((ders) => {
+
+//     console.log("Taranan Ders", ders);
+
+//     if (ders == "C#") return true;
+
+// });
+
+// if (cSharpIndex != -1) {
+//     console.log("C# DErsi Bulundu : ", cSharpIndex);
+// } else {
+// console.log("C# Dersi Bulunamadı");
+// }
+
+const yas = 100;
+
+if (yas <= 15){
+    console.log("Yaşınız 18 veya 18'den Küçüktür");
+} else if (yas <= 35){
+    console.log("Yaşınız 18 ve 35 Arasındadır");
+} else {
+    console.log("Yaşınız 35'den Büyüktür");
+
+}
+
+// video dk 30.27 dedyim konu Döngülerde Kaldım
